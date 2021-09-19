a0551e9d275c28808bffff798f3fccd74c2650e2
Rishika Dhanawade <rishika.dhanawade@gmail.com>
2021-09-19 10:10:48 +0530

    Update Footer.js

--- FrontEnd/emart/src/components/Footer.js
+++ FrontEnd/emart/src/components/Footer.js
@@ -24,10 +24,10 @@ export class Footer extends Component {
                 </div> */}
                  <div className="container-fluid" style={{backgroundColor: '#343a40'}}>
                 <ul className="nav justify-content-center border-bottom pb-3 mb-">
-                        <li className=""><a href="/"  className="nav-link px-2 text-muted">Home</a></li>
-                        <li className=""><a href="/contact" className="nav-link px-2 text-muted">ContactUs</a></li>
-                        <li className=""><a href="/About" className="nav-link px-2 text-muted">About</a></li>
-                        <li className=""><p className="nav-link px-2 text-muted">© 2021 eMART.com</p></li>
+                        <li className=""><a href="/"  className="nav-link px-2 text-light">Home</a></li>
+                        <li className=""><a href="/contact" className="nav-link px-2 text-light">ContactUs</a></li>
+                        <li className=""><a href="/About" className="nav-link px-2 text-light">About</a></li>
+                        <li className=""><p className="nav-link px-2 text-light">© 2021 eMART.com</p></li>
                 </ul>
                 </div>
             </header>
