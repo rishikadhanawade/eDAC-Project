e98ac16bfe4fb8a0f50b4fb668f15ad56ded1632
Shubham <13shubhamggupta@gmail.com>
2021-09-20 19:34:36 +0530

    footer

--- FrontEnd/emart/src/components/Footer1.js
+++ FrontEnd/emart/src/components/Footer1.js
@@ -2,38 +2,31 @@ import React, { Component } from 'react'
 // import { Link } from 'react-router-dom'
 import './css/Header.css'
 import { DataContext } from './Context'
+import { Footer } from 'react-bootstrap'
 import './css/Form.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
  
-export class Footer extends Component {
+export class Footer1 extends Component {
     static contextType = DataContext;
     render() {
         return (
             <header>
-                {/* <div className="footer-container" style={{backgroundColor: '#343a40'}} >
-                    <footer className>
-                        <ul className="nav justify-content-center border-bottom pb-3 mb-">
-                        <li className="nav-item" ><a href="/" className="nav-link px-2 text-muted">Home</a></li>
-                        <li className="nav-item"><a href="/contact" className="nav-link px-2 text-muted">ContactUs</a></li>
-                        <li className="nav-item"><a href="/About" className="nav-link px-2 text-muted">About</a></li>
-                        <li className="nav-item"><p className="nav-link px-2 text-muted">© 2021 Company, Inc</p></li>
-                        </ul>
-                    </footer>
-                </div> */}
+                <footer className="footer-container fixed-bottom">
                  <div className="container-fluid" style={{backgroundColor: '#343a40'}}>
-                <ul className="nav justify-content-center border-bottom pb-3 mb-">
+                <ul className="nav justify-content-center footer-ul">
                         <li className=""><a href="/"  className="nav-link px-2 text-light">Home</a></li>
                         <li className=""><a href="/contact" className="nav-link px-2 text-light">ContactUs</a></li>
                         <li className=""><a href="/About" className="nav-link px-2 text-light">About</a></li>
                         <li className=""><p className="nav-link px-2 text-light">© 2021 eMART.com</p></li>
                 </ul>
                 </div>
+                </footer>
             </header>
            
         )
     }
 } 
 
-export default Footer
+export default Footer1
