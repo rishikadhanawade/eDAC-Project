88528265b9aae20fea4f4ebbc5686524250a0098
SatyamVaish <satyam.vaish4@gmail.com>
2021-09-24 21:41:04 +0530

    Update About.js

--- FrontEnd/emart/src/components/About.js
+++ FrontEnd/emart/src/components/About.js
@@ -7,15 +7,14 @@ export default class About extends React.Component {
     return (
       <div id='about'>
           <h1 style={{margin:'0 0 25px 0'}}>About</h1>
-       <p> Welcome to [store name], your number one source for all things [product, ie: shoes, bags, dog treats]. 
-         We're dedicated to giving you the very best of [product], with a focus on [three characteristics, ie: dependability, customer service and uniqueness.]
-         Founded in [year] by [founder's name], [store name] has come a long way from its beginnings in a [starting location, ie: home office, toolshed, Houston, TX.]. 
-         When [store founder] first started out, his/her passion for [passion of founder, ie: helping other parents be more eco-friendly, providing the best equipment for his fellow musicians] 
-         drove him to [action, ie: do intense research, quit her day job], and gave him the impetus to turn hard work and inspiration into to a booming online store. 
-         We now serve customers all over [place, ie: the US, the world, the Austin area], and are thrilled to be a part of the [adjective, ie: quirky, eco-friendly, fair trade] 
-         wing of the [industry type, ie: fashion, baked goods, watches] industry.
+       <p> Welcome to eMart, your number one source for all things AllProducts
+         We're dedicated to giving you the very best of product, with a focus on three characteristics, ie: dependability, customer service and uniqueness.
+         Founded in 2021 by 7-Wonderz has come a long way from its beginnings in a starting location, ie: Mumbai, Maharashtra, India 
+         When we first started out, drove us to do intense research, and gave us the impetus to turn hard work and inspiration into to a booming online store. 
+         We now serve customers all over Mumbai,India  and are thrilled to be a part of the fair trade
+         wing of the industry type, ie: Electronics, Furnitures, fashion, Sports Equipment, watches .
          We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don't hesitate to contact us.
-         <p>Sincerely,<br/>Name, [title, ie: CEO, Founder, etc.</p>
+         <p>Sincerely,<br/> 7 Wonderz, e-Volutioners eDAC- May 2021</p>
         </p>
       </div>
     );
